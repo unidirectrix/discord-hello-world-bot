b96132c66264556d46acd4d5f4ae003266f54beb
PancakesForRent <astenbitancur@gmail.com>
2025-09-13 23:25:50 +0800

    Add utility functions

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,20 @@
 import 'dotenv/config';
 import express from 'express';
+import { 
+    verifyKeyMiddleware,
+    InteractionResponseFlags,
+    InteractionResponseType,
+    MessageComponentTypes
+} from 'discord-interactions';
 
 
-console.log(process.env);
+const app = express();
+const PORT = 3000;
+
+// Interactions endpoint URL where Discord will send HTTP requests
+// Parse request body and verify using the verifyKeyMiddleware from discord-interactions
+
+
+app.listen(PORT,  () => {
+    console.log('Listening on port', PORT);
+});
---
+++ utils.js
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+
+export async function DiscordRequest(endpoint, options) {
+    // API endpoint url + endpoint append
+    const url = 'https://discord.com/api/v10/' + endpoint;
+
+    // Stringify payloads
+    if (options.body) options.body = JSON.stringify(options.body);
+
+    // Use fetch to make requests
+    const res  = await fetch(url, {
+        headers: {
+            Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+            'Content-Type': 'application/json; charset=UTF-8',
+            'User-Agent': 'DiscordBot (, 1.0.0)',
+        },
+        ...options
+    });
+
+    // throw API errors
+    if (!res.ok) {
+        const data = await res.json();
+        console.log(res.status);
+        throw new Error(JSON.stringify(data));
+    }
+    
+    // return original response
+    return res;
+}
+
+export async function InstallGlobalCommand(appId, command) {
+    // API endpoint to overwrite global commands
+    const endpoint = `applications/${appId}/commands`;
+
+    try {
+        // Calling the create application command endpoint
+        // Testing for POST instead of bulk overwrite PUT
+        await DiscordRequest(endpoint, {method: 'POST', body: command});
+    } catch (err) {
+        console.log(err);
+    }
+}
