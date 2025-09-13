e74e104f99d0c56164e165294bea16bf67325d71
PancakesForRent <astenbitancur@gmail.com>
2025-09-14 00:18:27 +0800

    Install Hello World command to API

--- commands.js
+++ commands.js
@@ -1,10 +1,12 @@
 import 'dotenv/config';
+import { InstallGlobalCommand } from './utils.js';
 
 const HELLO_WORLD = {
-    name: 'hello_world',
+    name: 'hello',
     description: "Greets you hello world",
     type: 1,
     integration_types: [0, 1],
     contexts: [0, 1, 2], 
 };
 
+InstallGlobalCommand(process.env.APP_ID, HELLO_WORLD);
