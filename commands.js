10ce256a3b6ff481ea2a7c46cf3281406267caff
PancakesForRent <astenbitancur@gmail.com>
2025-09-13 23:30:18 +0800

    Add greeting command

---
+++ commands.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+
+const HELLO_WORLD = {
+    name: 'hello_world',
+    description: "Greets you hello world",
+    type: 1,
+    integration_types: [0, 1],
+    contexts: [0, 1, 2], 
+};
+
